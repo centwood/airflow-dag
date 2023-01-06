d03bd60d7eae152c6226fd8a53bf6c0e94e9dce3
centwood <108471026+centwood@users.noreply.github.com>
2023-01-06 10:29:24 -0600

    Add files via upload

---
+++ dags/cloud_iqx_runtime_psql_dag.py
@@ -0,0 +1,66 @@
+from sys import exec_prefix
+#from airflow import DAGhelmhelm
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+#from python.papers_staging import papers_etl, progressbar
+from datetime import datetime
+from airflow.utils.dates import days_ago
+from kubernetes import client, config
+import base64
+import os
+from airflow.kubernetes.secret import Secret 
+#from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.configuration import conf
+
+config.load_incluster_config()
+
+v1     = client.CoreV1Api()
+secret = v1.read_namespaced_secret("airflow-postgresdb", "airflow")
+data   = secret.data
+
+decodes = base64.b64decode(secret.data["pgurl"])
+pgurl = decodes.decode('utf-8')
+#namespace = conf.get("kubernetes","airflow")
+
+with DAG(dag_id='cloud_iqx_runtime_psql_dag',
+         default_args={'owner': 'airflow'},
+         schedule_interval='@daily',
+         start_date=days_ago(1)
+    ) as dag:
+
+    execute_sql_function_truncate_staging_table = PostgresOperator(
+        task_id='execute_sql_function_truncate_staging_table',
+        #AIRFLOW_CONN_POSTGRES_MASTER = pgurl,
+        postgres_conn_id='cloud_runtime_psql',
+        sql='''
+        call insert_cloud_runtime_job_test();
+        '''
+    )
+
+    execute_sql_function_insert_to_table = PostgresOperator(
+        task_id='execute_procedure_insert_cloud_runtime_job_test',
+        #AIRFLOW_CONN_POSTGRES_MASTER = pgurl,
+        postgres_conn_id='cloud_runtime_psql',
+        sql='''
+        call insert_cloud_runtime_job_test();
+        '''
+    )
+
+    execute_sql_function_update_working_table = PostgresOperator(
+        task_id='execute_sql_function_update_working_table',
+        #AIRFLOW_CONN_POSTGRES_MASTER = pgurl,
+        postgres_conn_id='cloud_runtime_psql',
+        sql='''
+        call insert_cloud_runtime_job_test();
+        '''
+    )
+
+    execute_sql_function_truncate_staging_table >> execute_sql_function_insert_to_table >> execute_sql_function_update_working_table
+
+    #postgres_conn_id = 'localPSQL',
+    #postgresql://postgres:L45hTHHxM7hhR46PJlIo@localhost:5432/postgres
